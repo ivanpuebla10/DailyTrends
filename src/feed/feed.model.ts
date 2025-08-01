40a316b982e78eb924f91ff8921f46f287cff23e
ivanpuebla10 <ivanpuebla94@gmail.com>
2025-08-01 17:52:25 +0200

    fix: feed model fixes

--- src/feed/feed.model.ts
+++ src/feed/feed.model.ts
@@ -1,19 +1,27 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema, Types } from 'mongoose';
 
 export interface IFeed extends Document {
+  _id: Types.ObjectId;
+  id: string;
   title: string;
   summary: string;
-  source: string;
   publicationDate: Date;
   creationDate: Date;
 }
 
 const FeedSchema: Schema = new Schema<IFeed>({
+  id: { type: String, unique: true },
   title: { type: String, required: true },
   summary: { type: String },
-  source: { type: String, enum: ['El País', 'El Mundo', 'Custom'], required: true },
   publicationDate: { type: Date, required: true },
   creationDate: { type: Date, default: Date.now },
+});
+
+FeedSchema.pre('save', function (this: IFeed, next) {
+  if (!this.id) {
+    this.id = this._id.toString();
+  }
+  next();
 });
 
 export default mongoose.model<IFeed>('Feed', FeedSchema);
